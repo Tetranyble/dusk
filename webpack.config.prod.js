8185cd555e87883afaa09191526c58780d0131f9
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2020-09-08 05:06:49 +0100

    build script added but not working

---
+++ webpack.config.prod.js
@@ -0,0 +1,90 @@
+import path from 'path';
+//import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  mode: 'production',
+
+  devtool: 'source-map',
+
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+
+
+
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: '43ad216f57d94259968435894490a5c7'
+    })
+
+    // Eliminate duplicate packages when generating bundle
+
+
+    // Minify JS
+
+  ],
+  // Use CommonsChunkPlugin to create a separate bundle
+  // of vendor libraries so that they're cached separately.
+  optimization: {
+    minimize: true,
+    runtimeChunk: true,
+    splitChunks: {
+        chunks: "async",
+        minSize: 1000,
+        minChunks: 2,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        name: true,
+        cacheGroups: {
+            default: {
+                minChunks: 1,
+                priority: -20,
+                reuseExistingChunk: true,
+            },
+            vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10
+            }
+        }
+    }
+},
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
+      {test: /\.css$/, use: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+};
