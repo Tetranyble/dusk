ddca1a659fb36ff73503f19958a74cc2b12da56a
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2020-09-10 22:16:16 +0100

    added procfile back

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const compression = require('compression');
 
 /* eslint-disable no-console */
 
-const port = 3000;
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(compression());
@@ -16,10 +16,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('server now running at: http://localhost:' + port);
-  }
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
