19e528443b330e209b941addc0beac612ce671b0
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2020-09-10 20:56:59 +0100

    favicon and manifest added for web app configuration

--- src/index.js
+++ src/index.js
@@ -1,35 +1,48 @@
 import './index.css';
 
-import {getUsers, deleteUser} from './api/userApi';
+const api = {
+  key: "10451c9691d994efc824cfee92009ea0",
+  base: "https://api.openweathermap.org/data/2.5/weather?"
+}
 
-// Populate table of users via API call.
-getUsers().then(result => {
-  let usersBody = "";
+const setQuery = (evt) =>{
+
+  if(evt.keyCode == 13){
+    getResults(searchbox.value)
+  }
+}
+const dateBuilder = (now) => {
+  let months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
+return `${days[now.getDay()]}, ${months[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()}`
+}
+const displayResults = (weather) => {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now)
+
+  let temp = document.querySelector('.current .temperature');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>C</span>`;
+  console.log(weather)
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].description;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${weather.main.temp_min}C / ${weather.main.temp_max} C`
+}
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+//&exclude={minutely,hourly}
+const getResults = (query) => {
+  fetch(`${api.base}q=${query}&units=metric&APPID=${api.key}`)
+  .then( (weather) => {
+    return weather.json()
+  }).then(displayResults)
+}
 
 
-  result.forEach(user => {
-    usersBody+= `<tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-      <td>${user.id}</td>
-      <td>${user.firstName}</td>
-      <td>${user.lastName}</td>
-      <td>${user.email}</td>
-      </tr>`
-  });
-
-  global.document.getElementById('users').innerHTML = usersBody;
-
-  const deleteLinks = global.document.getElementsByClassName('deleteUser');
-
-  // Must use array.from to create a real array from a DOM collection
-  // getElementsByClassname only returns an "array like" object
-  Array.from(deleteLinks, link => {
-    link.onclick = function(event) {
-      const element = event.target;
-      event.preventDefault();
-      deleteUser(element.attributes["data-id"].value);
-      const row = element.parentNode.parentNode;
-      row.parentNode.removeChild(row);
-    };
-  });
-});
